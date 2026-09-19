4f9d5038808b023d7dbb3398ed820b738184b2a4
Василий Крюков <SOLO144444@yandex.ru>
2026-09-19 13:13:12 +0700

    Счетчик

--- app.py
+++ app.py
@@ -39,3 +39,18 @@ def image():
     </body> 
 </html>
 '''
+
+count = 0
+
+@app.route('/counter')
+def counter():
+    global count
+    count += 1
+    return '''
+<!doctype html>
+<html>
+    <body> 
+        СКолько раз вы сюда заходили: ''' + str(count) +'''
+    </body> 
+</html>
+'''
