0bf2a86099b6b06722225cef9b8f11b6123d9ea0
Василий Крюков <SOLO144444@yandex.ru>
2026-09-24 02:56:57 +0700

    Главная страница

--- app.py
+++ app.py
@@ -3,6 +3,32 @@ import datetime
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
+def index():
+    return """<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body> 
+        <header>
+            <h1>НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных</h1>
+        </header>
+        
+        <main>
+            <menu>
+                <li><a href="/lab1">Первая лабораторная</a></li>
+            </menu>
+        </main>
+        
+        <footer>
+            <hr>
+            <p>Крюков Василий Александрович, ФБИ-41, 3 курс, 2026 год</p>
+        </footer>
+    </body>
+</html>"""
+
+@app.route("/lab1")
 @app.route("/lab1/web")
 def web():
     return """<!doctype html>
@@ -10,6 +36,7 @@ def web():
             <body> 
                 <h1>web-сервер на flask</h1>
                 <a href="/lab1/author">author</a>
+                <a href="/index">На главную</a>
             </body>
         </html>""", 200, {
             "X-Server": "sample",
@@ -104,3 +131,4 @@ def created():
 @app.errorhandler(404)
 def not_found(err):
     return "нет такой страницы", 404
+
